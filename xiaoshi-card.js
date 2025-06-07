62ff4966e9e46dded42473a31e31d9365e4d4341
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-06-07 16:23:42 +0800

    Update xiaoshi-card.js

--- xiaoshi-card.js
+++ xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝集合卡 \n%c   v 2.2.7  ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
+console.info("%c 消逝集合卡 \n%c   v 2.2.8  ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
 import { LitElement, html, css } from 'https://unpkg.com/lit-element@2.4.0/lit-element.js?module';
 
 class XiaoshiLightCard extends LitElement {
@@ -2425,7 +2425,7 @@ class XiaoshiStateGrid1Card extends LitElement {
         text-overflow: ellipsis;
         width: 100%;
         text-align: center;
-        margin-top: 0px;
+        margin-top: -4px;
       }
       
       .data-item-name {
@@ -2435,6 +2435,7 @@ class XiaoshiStateGrid1Card extends LitElement {
         text-overflow: ellipsis;
         width: 100%;
         text-align: center;
+        margin-top: 2px;
       }
       
       .warning {
@@ -2766,7 +2767,7 @@ class XiaoshiStateGrid2Card extends LitElement {
         text-overflow: ellipsis;
         width: 100%;
 				text-align: center;
-        margin-top: 0px;
+        margin-top: -4px;
       }
        
       .data-item-name {
@@ -2776,6 +2777,7 @@ class XiaoshiStateGrid2Card extends LitElement {
         text-overflow: ellipsis;
         width: 100%;
 				text-align: center;
+        margin-top: 2px;
       }
       
       .warning {
