3e7bc07941010da0e5025b9b2ba0664053c0b445
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-06-08 20:41:33 +0800

    Update xiaoshi-card.js

--- xiaoshi-card.js
+++ xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝集合卡 \n%c   v 2.3.3  ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
+console.info("%c 消逝集合卡 \n%c   v 2.3.4  ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
 import { LitElement, html, css } from 'https://unpkg.com/lit-element@2.4.0/lit-element.js?module';
 
 class XiaoshiLightCard extends LitElement {
@@ -2680,7 +2680,7 @@ class XiaoshiStateGrid1Card extends LitElement {
     `;
   }
 }
-customElements.define('xiaoshi-state-grid1-card', );
+customElements.define('xiaoshi-state-grid1-card', XiaoshiStateGrid1Card);
 
 class XiaoshiStateGrid2Card extends LitElement {
   static get properties() {
