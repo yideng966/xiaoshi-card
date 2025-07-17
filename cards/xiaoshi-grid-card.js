be8eb55ea37e7588d89b62f0dc1eb58ba99a2095
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-07-17 14:09:55 +0800

    Update xiaoshi-grid-card.js

--- cards/xiaoshi-grid-card.js
+++ cards/xiaoshi-grid-card.js
@@ -1,52 +1,52 @@
 import { LitElement, html, css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 export class XiaoshiGridCard extends LitElement {
-  static get properties() {
-    return {
-      hass: Object,
-      config: Object,
-    };
-  }
+	static get properties() {
+		return {
+			hass: Object,
+			config: Object,
+		};
+	}
 
-  static get styles() {
-    return css`
-      .container {
-        position: relative;
-        display: block;
-        overflow: hidden;
-      }
-      .grid-item {
-        position: absolute;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: white;
-        box-sizing: border-box;
-        border: 0;
+	static get styles() {
+		return css`
+			.container {
+				position: relative;
+				display: block;
+				overflow: hidden;
+			}
+			.grid-item {
+				position: absolute;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				color: white;
+				box-sizing: border-box;
+				border: 0;
 				cursor: pointer;
-      }
-    `;
-  }
+			}
+		`;
+	}
 
-  setConfig(config) {
-    if (!config.entities) {
-      throw new Error('You need to define entities');
-    }
-    this.config = {
-      width: config.width || '400px',
-      height: config.height || '80px',
-      min: config.min || 0,
-      max: config.max || 100,
-      mode: config.mode || '温度',
-      display: config.display || false,
-      entities: config.entities.map(entity => ({
-        ...entity,
-        state: entity.state !== false,
-      })),
-    };
-  }
+	setConfig(config) {
+		if (!config.entities) {
+			throw new Error('You need to define entities');
+		}
+		this.config = {
+			width: config.width || '400px',
+			height: config.height || '80px',
+			min: config.min || 0,
+			max: config.max || 100,
+			mode: config.mode || '温度',
+			display: config.display || false,
+			entities: config.entities.map(entity => ({
+				...entity,
+				state: entity.state !== false,
+			})),
+		};
+	}
 
-  render() {
+	render() {
 		if(this._display()) return;
 		return html`
 			<div class="container"\n
@@ -66,9 +66,9 @@ export class XiaoshiGridCard extends LitElement {
 					}
 					let size = Number(grid[2].slice(0, grid[2].length-1));
 					let fsize ="11px";
-					if (size<25 )  fsize ="10px";
-					if (size<20 )  fsize ="9px";
-					if (size<15 )  fsize ="8px";
+					if (size<25 )	fsize ="10px";
+					if (size<20 )	fsize ="9px";
+					if (size<15 )	fsize ="8px";
 					return html`
 						<div 
 							class="grid-item"\n
@@ -116,7 +116,7 @@ export class XiaoshiGridCard extends LitElement {
 		temp = parseFloat(temp);
 		const { min, max } = this.config;
 		let deg;
-		  
+			
 		if (temp > 25) {
 			deg = (25 - temp) * 120 / (max - 25);
 		} else {
@@ -140,8 +140,8 @@ export class XiaoshiGridCard extends LitElement {
 		return `hue-rotate(${deg}deg)`;
 	}
 
-  getCardSize() {
-    return 1;
-  }
+	getCardSize() {
+		return 1;
+	}
 }
 customElements.define('xiaoshi-grid-card', XiaoshiGridCard);
