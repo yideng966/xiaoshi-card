b7a58e8fa9493cbba73a084c5ebc3381b77bbfc0
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-07-19 22:43:42 +0800

    Update xiaoshi-card.js

--- xiaoshi-card.js
+++ xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝集合卡 \n%c   v 2.7.9 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
+console.info("%c 消逝集合卡 \n%c   v 2.8.0 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
 
 import { cardConfigs } from './xiaoshi-config.js';
 
